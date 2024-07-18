c645297b714fad344aec88f6e91090b16c4eac64
pdmarkovits <panagiotis.markovits@athenarc.gr>
2024-07-18 05:01:36 +0300

    - Added DOI mode tab in gradio app

--- src/citance_analysis/server/gradio_app.py
+++ src/citance_analysis/server/gradio_app.py
@@ -57,9 +57,15 @@ with gr.Blocks() as pdf_analysis:
     pdf_output = gr.JSON(label="Output")
     process_pdf_button.click(analyze_pdf, inputs=[pdf_input], outputs=[pdf_output])
 
+# Define the interface for the third tab (DOI Mode)
+with gr.Blocks() as doi_mode:
+    gr.Markdown("### SciNoBo Citance Analysis - DOI Mode")
+    doi_input = gr.Textbox(label="DOI", placeholder="Enter a valid Digital Object Identifier", interactive=False)
+    gr.HTML("<span style='color:red;'>This functionality is not ready yet.</span>")
+
 # Combine the tabs into one interface
 with gr.Blocks() as demo:
-    gr.TabbedInterface([text_analysis, pdf_analysis], ["Text Mode", "PDF Mode"])
+    gr.TabbedInterface([text_analysis, pdf_analysis, doi_mode], ["Text Mode", "PDF Mode", "DOI Mode"])
 
 # Launch the interface
 demo.queue().launch()
