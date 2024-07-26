786eafffe9da42f17d411791f8aa789d938d8cf3
Petros Stavropoulos <petros.stavropoulos@athenarc.gr>
2024-07-26 23:22:33 +0300

    fix doi mode

--- src/citance_analysis/server/gradio_app.py
+++ src/citance_analysis/server/gradio_app.py
@@ -2,7 +2,8 @@
 import os
 import gradio as gr
 import requests as req
-from citance_analysis.pipeline.inference import extract_citances_pis, find_mark_pis, find_polarity, find_intent, find_semantics
+from tqdm import tqdm
+from citance_analysis.pipeline.inference import extract_citances_pis, find_mark_pis, find_mark_pis_parquet, find_polarity, find_intent, find_semantics
 
 # Retrieve HF space secrets
 BACKEND_IP = os.getenv('BACKEND_IP')
@@ -49,9 +50,7 @@ def analyze_pdf(pdf_file, progress=gr.Progress(track_tqdm=True)):
     return results
 
 
-def analyze_input_doi(
-    doi: str | None
-):
+def analyze_input_doi(doi: str | None, progress=gr.Progress(track_tqdm=True)):
     if (doi is None):
         results = {'error': 'Please provide the DOI of the publication'}
         return results
@@ -62,11 +61,22 @@ def analyze_input_doi(
         url = f"http://{BACKEND_IP}:{BACKEND_PORT}{BACKEND_PATH}{doi}"
         response = req.get(url)
         response.raise_for_status()
-        results = response.json()
-        return results
+        # Get the data
+        data = response.json()
+
+        # Extract the citances and PIs
+        results = []
+        for citance in tqdm(data['context'], desc='Processing Citances'):
+            res = find_mark_pis_parquet(citance)
+            results.append({
+                'citance': citance,
+                'results': res
+            })
+
+        return data, results
     except Exception as e:
         results = {'error': str(e)}
-    return results
+        return results, results
 
 # Define the interface for the first tab (Text Analysis)
 with gr.Blocks() as text_analysis:
@@ -74,7 +84,9 @@ with gr.Blocks() as text_analysis:
     citance_input = gr.Textbox(label="Citance")
     citation_mark_input = gr.Textbox(label="Citation Mark", placeholder="Enter citation mark or leave blank")
     process_text_button = gr.Button("Process")
+
     text_output = gr.JSON(label="Output")
+
     process_text_button.click(analyze_text, inputs=[citance_input, citation_mark_input], outputs=[text_output])
 
 # Define the interface for the second tab (PDF Analysis)
@@ -82,7 +94,9 @@ with gr.Blocks() as pdf_analysis:
     gr.Markdown("### SciNoBo Citance Analysis - PDF Mode")
     pdf_input = gr.File(label="Upload PDF", file_types=[".pdf"])
     process_pdf_button = gr.Button("Process")
+
     pdf_output = gr.JSON(label="Output")
+
     process_pdf_button.click(analyze_pdf, inputs=[pdf_input], outputs=[pdf_output])
 
 # Define the interface for the third tab (DOI Mode)
@@ -90,8 +104,11 @@ with gr.Blocks() as doi_mode:
     gr.Markdown("### SciNoBo Citance Analysis - DOI Mode")
     doi_input = gr.Textbox(label="DOI", placeholder="Enter a valid Digital Object Identifier")
     process_doi_button = gr.Button("Process")
+
+    doi_metadata = gr.JSON(label="DOI Metadata")
     doi_output = gr.JSON(label="Output")
-    process_doi_button.click(analyze_input_doi, inputs=[doi_input], outputs=[doi_output])
+    
+    process_doi_button.click(analyze_input_doi, inputs=[doi_input], outputs=[doi_metadata, doi_output])
 
 # Combine the tabs into one interface
 with gr.Blocks() as demo:
